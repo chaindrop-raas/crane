f608aef30c00ba58ee683c69a9ad0f8e380d8197
Stephen Caudill <scaudill@gmail.com>
2022-09-23 09:36:32 -0600

    Add membership token factory tests

---
+++ test/OrigamiMembershipTokenFactory.t.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: ITSATEST
+pragma solidity 0.8.9;
+
+import "forge-std/Test.sol";
+import "src/OrigamiMembershipToken.sol";
+import "src/OrigamiMembershipTokenFactory.sol";
+import "@oz/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@oz/proxy/transparent/ProxyAdmin.sol";
+
+abstract contract AddressHelper {
+    address admin = address(0x1);
+    address owner = address(0x2);
+    address rando = address(0x3);
+}
+
+abstract contract OMTFHelper is AddressHelper, Test {
+    OrigamiMembershipTokenFactory factoryImpl;
+    TransparentUpgradeableProxy factoryProxy;
+    OrigamiMembershipTokenFactory factory;
+    ProxyAdmin factoryAdmin;
+    OrigamiMembershipToken token;
+
+    function setUp() public {
+        vm.startPrank(admin);
+        factoryAdmin = new ProxyAdmin();
+        factoryImpl = new OrigamiMembershipTokenFactory();
+        factoryProxy = new TransparentUpgradeableProxy(
+            address(factoryImpl),
+            address(factoryAdmin),
+            ""
+        );
+        factory = OrigamiMembershipTokenFactory(address(factoryProxy));
+        factory.initialize();
+
+        address tokenProxyAddress = factory.createOrigamiMembershipToken(
+            owner,
+            "Factory Membership Token",
+            "FMT",
+            "ipfs://deadbeef/"
+        );
+        token = OrigamiMembershipToken(tokenProxyAddress);
+        vm.stopPrank();
+        vm.startPrank(owner);
+    }
+}
+
+contract DeployingMembershipTokenFactoryTest is AddressHelper, Test {
+    OrigamiMembershipTokenFactory factoryImpl;
+    TransparentUpgradeableProxy factoryProxy;
+    OrigamiMembershipTokenFactory factory;
+    ProxyAdmin factoryAdmin;
+    OrigamiMembershipToken token;
+
+    event OrigamiMembershipTokenCreated(
+        address indexed caller,
+        address indexed proxy
+    );
+
+    function setUp() public {
+        vm.startPrank(admin);
+
+        factoryAdmin = new ProxyAdmin();
+        factoryImpl = new OrigamiMembershipTokenFactory();
+        factoryProxy = new TransparentUpgradeableProxy(
+            address(factoryImpl),
+            address(factoryAdmin),
+            ""
+        );
+        factory = OrigamiMembershipTokenFactory(address(factoryProxy));
+        factory.initialize();
+    }
+
+    function testCreateEmitsEvent() public {
+        // we skip validating argument 2 because the address is not known until after the create call
+        vm.expectEmit(true, false, true, true, address(factory));
+
+        emit OrigamiMembershipTokenCreated(admin, address(0x0));
+
+        address tokenProxyAddress = factory.createOrigamiMembershipToken(
+            owner,
+            "Factory Membership Token",
+            "FMT",
+            "ipfs://deadbeef/"
+        );
+        token = OrigamiMembershipToken(tokenProxyAddress);
+        vm.stopPrank();
+        vm.prank(owner);
+        assertEq(token.name(), "Factory Membership Token");
+        assertEq(token.symbol(), "FMT");
+    }
+}
+
+contract AccessControlForMembershipTokenFactoryTest is OMTFHelper {
+    function testOnlyAdminCanCreate() public {
+        vm.expectRevert(
+            bytes(
+                "AccessControl: account 0x0000000000000000000000000000000000000002 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000"
+            )
+        );
+        factory.createOrigamiMembershipToken(
+            owner,
+            "Factory Membership Token",
+            "FMT",
+            "ipfs://deadbeef/"
+        );
+    }
+
+    function testCanGrantRoleOnFactoryIfAdmin() public {
+        vm.stopPrank();
+        vm.startPrank(admin);
+        factory.grantRole(factory.DEFAULT_ADMIN_ROLE(), rando);
+        assertEq(factory.hasRole(factory.DEFAULT_ADMIN_ROLE(), rando), true);
+    }
+
+    function testCanGrantRoleOnInstanceIfOwner() public {
+        token.grantRole(token.DEFAULT_ADMIN_ROLE(), rando);
+        assertEq(token.hasRole(token.DEFAULT_ADMIN_ROLE(), rando), true);
+    }
+}
+
+contract MembershipTokenFactoryProxyAddressTest is OMTFHelper {
+    function testProxyAddressIsCorrect() public {
+        assertEq(
+            address(token),
+            address(factory.getProxyContractAddress(0))
+        );
+    }
+
+    function testCannotGetProxyAddressOutOfIndex() public {
+        vm.expectRevert(bytes("Proxy address index out of bounds"));
+        factory.getProxyContractAddress(1);
+    }
+}
