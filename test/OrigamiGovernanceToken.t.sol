58772fb7056240dca49c3f866be15d4532a4bc41
Stephen Caudill <scaudill@gmail.com>
2022-09-23 17:20:34 -0600

    Test upgrading the governance token

--- test/OrigamiGovernanceToken.t.sol
+++ test/OrigamiGovernanceToken.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.9;
 
 import "forge-std/Test.sol";
 import "src/OrigamiGovernanceToken.sol";
+import "src/versions/OrigamiGovernanceTokenBeforeInitialAuditFeedback.sol";
 import "@oz/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@oz/proxy/transparent/ProxyAdmin.sol";
 
@@ -78,5 +79,66 @@ contract DeployGovernanceTokenTest is AddressHelper, Test {
             "DTDG",
             10000000000000000000000000000
         );
+    }
+}
+
+contract UpgradeGovernanceTokenTest is Test, AddressHelper {
+    OrigamiGovernanceTokenBeforeInitialAuditFeedback implV1;
+    OrigamiGovernanceToken implV2;
+    TransparentUpgradeableProxy proxy;
+    OrigamiGovernanceTokenBeforeInitialAuditFeedback tokenV1;
+    OrigamiGovernanceToken tokenV2;
+    ProxyAdmin admin;
+
+    event TransferEnabled(address indexed caller, bool value);
+
+    function setUp() public {
+        admin = new ProxyAdmin();
+        implV1 = new OrigamiGovernanceTokenBeforeInitialAuditFeedback();
+        proxy = new TransparentUpgradeableProxy(
+            address(implV1),
+            address(admin),
+            ""
+        );
+        tokenV1 = OrigamiGovernanceTokenBeforeInitialAuditFeedback(
+            address(proxy)
+        );
+
+        tokenV1.initialize(
+            owner,
+            "Deciduous Tree DAO Governance",
+            "DTDG",
+            10000000000000000000000000000
+        );
+    }
+
+    function testCanInitialize() public {
+        assertEq(tokenV1.name(), "Deciduous Tree DAO Governance");
+    }
+
+    function testCannotInitializeTwice() public {
+        vm.expectRevert(
+            bytes("Initializable: contract is already initialized")
+        );
+        tokenV1.initialize(
+            owner,
+            "EVEN MOAR Deciduous Tree DAO Governance",
+            "EMDTDG",
+            10000000000000000000000000000
+        );
+    }
+
+    function testCanUpgrade() public {
+        implV2 = new OrigamiGovernanceToken();
+        admin.upgrade(proxy, address(implV2));
+        tokenV2 = OrigamiGovernanceToken(address(proxy));
+        vm.prank(owner);
+
+        vm.expectEmit(true, true, true, true, address(tokenV2));
+
+        // TransferEnabled does not exist in tokenV1, so it being emited here is proof that the upgrade worked
+        emit TransferEnabled(owner, true);
+
+        tokenV2.enableTransfer();
     }
 }
