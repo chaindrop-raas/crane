a938261b95447ca44f1d3d0407c4a4166e0c30a2
Stephen Caudill <scaudill@gmail.com>
2022-11-30 18:31:07 -0700

    Don't reserve space for a gap
    We'll do this if it seems like we need to when deploying.

--- src/governor/SimpleCounting.sol
+++ src/governor/SimpleCounting.sol
@@ -107,11 +107,4 @@ abstract contract SimpleCounting is GovernorWithProposalParams {
     function _quadraticWeight(uint256 weight) public pure returns (uint256) {
         return _squareRoot(weight);
     }
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[49] private __gap;
 }
