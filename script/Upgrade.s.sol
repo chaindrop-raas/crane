d0e5cd1b92403370ad5e5f4c76ae62a7f41692f8
Stephen Caudill <scaudill@gmail.com>
2022-12-29 14:39:54 -0700

    Add upgrade scripts for gov and mem tokens

---
+++ script/Upgrade.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+
+import "@std/Script.sol";
+import "src/OrigamiGovernanceToken.sol";
+import "src/OrigamiMembershipToken.sol";
+import "@oz/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@oz/proxy/transparent/ProxyAdmin.sol";
+
+contract UpgradeScript is Script {
+
+    function upgradeGovernanceToken(address proxyAdmin, address payable transparentProxy) public {
+        OrigamiGovernanceToken newImpl;
+        TransparentUpgradeableProxy proxy;
+        ProxyAdmin admin;
+
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        newImpl = new OrigamiGovernanceToken();
+        admin = ProxyAdmin(proxyAdmin);
+        proxy = TransparentUpgradeableProxy(transparentProxy);
+        admin.upgrade(proxy, address(newImpl));
+
+        vm.stopBroadcast();
+    }
+
+    function upgradeMembershipToken(address proxyAdmin, address payable transparentProxy) public {
+        OrigamiMembershipToken newImpl;
+        TransparentUpgradeableProxy proxy;
+        ProxyAdmin admin;
+
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        newImpl = new OrigamiMembershipToken();
+        admin = ProxyAdmin(proxyAdmin);
+        proxy = TransparentUpgradeableProxy(transparentProxy);
+        admin.upgrade(proxy, address(newImpl));
+
+        vm.stopBroadcast();
+    }
+}
