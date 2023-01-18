128768554abdc272f5a7a2baade703341b67ac75
Stephen Caudill <scaudill@gmail.com>
2023-01-18 10:25:57 -0700

    More moving around of facet tests

--- test/governor/GovernorSettingsFacet.t.sol
+++ test/governor/GovernorSettingsFacet.t.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: ITSATEST
 pragma solidity 0.8.16;
 
-import { GovernorDiamondHelper } from "test/OrigamiDiamondTestHelper.sol";
-
 import "src/interfaces/IGovernor.sol";
+import {GovernorDiamondHelper} from "test/OrigamiDiamondTestHelper.sol";
 
-contract OrigamiGovernorConfigTest is GovernorDiamondHelper {
+contract SettingsFacetTest is GovernorDiamondHelper {
+    function testInformationalFunctions() public {
+        assertEq(settingsFacet.votingDelay(), 604_800);
+        assertEq(settingsFacet.votingPeriod(), 604_800);
+        assertEq(settingsFacet.proposalThreshold(), 1);
+        assertEq(settingsFacet.quorumNumerator(), 10);
+    }
+
+    function testRetrieveProposalThreshold() public {
+        assertEq(settingsFacet.proposalThreshold(), 1);
+    }
+
     function testCannotDirectlyUpdateGovernanceToken() public {
         vm.expectRevert("Governor: onlyGovernance");
         settingsFacet.setGovernanceToken(address(0));
