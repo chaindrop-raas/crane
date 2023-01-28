15db37df7c198cf04945d81071ba347d0a2be4a2
Stephen Caudill <scaudill@gmail.com>
2023-01-28 15:26:35 -0700

    Formatting fix

--- script/Governor.s.sol
+++ script/Governor.s.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
-
 import "src/OrigamiTimelockController.sol";
 import "src/utils/DiamondDeployHelper.sol";
 import "src/utils/GovernorDiamondInit.sol";
