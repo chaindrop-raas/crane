1ce7a5f0be707d8040d47be398b2ceaa2cf09414
Stephen Caudill <scaudill@gmail.com>
2023-03-07 11:55:17 -0700

    Add scripts to deploy a new core facet and upgrade to it

--- script/Governor.s.sol
+++ script/Governor.s.sol
@@ -8,6 +8,17 @@ import "src/utils/GovernorDiamondInit.sol";
 import "@std/Script.sol";
 import "@diamond/Diamond.sol";
 
+contract DeployGovernorCoreFacet is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        GovernorCoreFacet governorCoreFacet = new GovernorCoreFacet();
+        console2.log("GovernorCoreFacet deployed at", address(governorCoreFacet));
+
+        vm.stopBroadcast();
+    }
+}
+
 contract DeployGovernorFacets is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -142,6 +153,67 @@ contract GovernorInstance is Script {
         vm.startBroadcast(adminPrivateKey);
         IDiamondCut.FacetCut[] memory cuts = facetCuts(config);
         DiamondCutFacet(governorDiamond).diamondCut(cuts, governorDiamondInit, encodeConfig(admin, timelock, config));
+        vm.stopBroadcast();
+    }
+}
+
+/**
+ * @dev this upgrades the diamond from a GovernorCoreFacet on `bf9c4b8` to one
+ * on `710a92a` and the substantive change is to add the `getAccountNonce` fn
+ */
+contract UpgradeDiamondFromV001ToV002 is Script {
+    function run(address governorDiamond, address newCoreFacet) external {
+        bytes4[] memory replaces = new bytes4[](32);
+        replaces[0] = 0xb08e51c0;
+        replaces[1] = 0xa217fddf;
+        replaces[2] = 0x253d2c7d;
+        replaces[3] = 0xb03e27fc;
+        replaces[4] = 0xa4785152;
+        replaces[5] = 0x56781388;
+        replaces[6] = 0xd17ad2d4;
+        replaces[7] = 0x7b3c71d3;
+        replaces[8] = 0xcd8514f6;
+        replaces[9] = 0x78e890ba;
+        replaces[10] = 0x248a9ca3;
+        replaces[11] = 0x4b187e34;
+        replaces[12] = 0x2f2ff15d;
+        replaces[13] = 0x91d14854;
+        replaces[14] = 0x43859632;
+        replaces[15] = 0xc59057e4;
+        replaces[16] = 0x06fdde03;
+        replaces[17] = 0xc01f9e37;
+        replaces[18] = 0x2d63f693;
+        replaces[19] = 0x544ffc9c;
+        replaces[20] = 0x7d5e81e2;
+        replaces[21] = 0x362697b7;
+        replaces[22] = 0xe5294480;
+        replaces[23] = 0x443e5a58;
+        replaces[24] = 0x6ce43248;
+        replaces[25] = 0x8a8c79cf;
+        replaces[26] = 0xf8ce560a;
+        replaces[27] = 0x36568abe;
+        replaces[28] = 0xd547741f;
+        replaces[29] = 0x6c4b0e9f;
+        replaces[30] = 0x3e4f49e6;
+        replaces[31] = 0x54fd4d50;
+
+        bytes4[] memory adds = new bytes4[](1);
+        adds[0] = 0xd126199f;
+
+        IDiamondCut.FacetCut[] memory cuts = new IDiamondCut.FacetCut[](2);
+        cuts[0] = IDiamondCut.FacetCut({
+            facetAddress: newCoreFacet,
+            action: IDiamondCut.FacetCutAction.Replace,
+            functionSelectors: replaces
+        });
+        cuts[1] = IDiamondCut.FacetCut({
+            facetAddress: newCoreFacet,
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: adds
+        });
+
+        vm.startBroadcast();
+        DiamondCutFacet(governorDiamond).diamondCut(cuts, address(0), "");
         vm.stopBroadcast();
     }
 }
